45c26c1c94d104f5b2da624adcc1e40cce1aadea
Showrove Roy <showrove200@gmail.com>
2022-09-08 14:55:42 +0600

    news popUp done

--- app.js
+++ app.js
@@ -64,8 +64,11 @@ const showPost = postList => {
     postList.forEach(post => {
         const postItem = document.createElement('div');
         postItem.classList.add('col');
+        postItem.setAttribute('onclick', `loadPostDetails('${post._id}')`);
+        postItem.setAttribute('data-bs-toggle', 'modal');
+        postItem.setAttribute('data-bs-target', '#newsPopUP');
         postItem.innerHTML = `
-        <div class="card mb-3" onclick="loadPostDetails('${post._id}')" data-bs-toggle="modal" data-bs-target="#newsPopUP">
+        <div class="card mb-3">
         <div class="row g-0">
             <div class="col-md-3">
                 <img src="${post.thumbnail_url}" class="img-fluid rounded-start" alt="...">
@@ -106,7 +109,7 @@ const showPost = postList => {
     });
 
 
-    //Post count and set
+    // 6 Post count and set
     const newsCount = postList.length;
     const setNewsNum = document.getElementById('news-count');
     setNewsNum.innerHTML = '';
@@ -129,10 +132,71 @@ const showPost = postList => {
     toggleSpinner(false);
 };
 
-// Post load Details 
+// 7 load news Details 
 const loadPostDetails = news_id => {
     const newsURL = `https://openapi.programming-hero.com/api/news/${news_id}`;
-}
+    fetch(newsURL)
+        .then(res => res.json())
+        .then(data => showNewsDetails(data.data[0]))
+        .catch(err => console.log(err));
+
+};
+
+// 8 show news details om modal 
+const showNewsDetails = newsDetailsList => {
+    const newsPopUPContainer = document.getElementById('newsPopUP-container');
+    newsPopUPContainer.innerHTML = '';
+    newsPopUPContainer.innerHTML = `
+    <!-- Modal -->
+    <div class="modal fade" id="newsPopUP" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">${newsDetailsList.title}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <div class="card mb-3">
+                        <img src="${newsDetailsList.image_url}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">${newsDetailsList.details}</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="modal-footer justify-content-between">
+
+
+                <div class="d-flex justify-content-between align-items-center">
+                <div class="d-flex align-items-center gap-2">
+                    <div>
+                        <img class="rounded-circle" src="${newsDetailsList.author.img}" alt="" width="50" height="50">
+                    </div>
+                    <div>
+                        <p class="p-0 m-0">${newsDetailsList.author.name}</p>
+                        <span class="text-muted">${newsDetailsList.author.published_date}</span>
+                    </div>
+                </div>
+                <div class="mx-3">
+                    <i class="fa-solid fa-eye"></i>
+                    <strong> ${newsDetailsList.total_view}K</strong>
+                </div>
+                <div class="rating-star fw-bold mx-3">
+                ${newsDetailsList.rating.number}
+                <i role="button" class="fa-solid fa-star"></i>
+                </div>
+            </div>
+
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    console.log(newsDetailsList);
+};
 
 
 //Loading Function 
